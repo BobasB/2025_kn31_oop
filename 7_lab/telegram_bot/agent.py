0c9a6f401657bffb263b046575a592902dfc3e04
BobasB <bugil.bogdan@gmail.com>
2026-04-03 10:26:44 +0300

    додали інструмент часу

--- 7_lab/telegram_bot/agent.py
+++ 7_lab/telegram_bot/agent.py
@@ -1,4 +1,11 @@
 from google.adk.agents.llm_agent import Agent
+from datetime import datetime
+
+def get_current_time() -> dict:
+    """Це інструмент для отримання поточного часу.
+        return: Словник з поточним часом у форматі "YYYY-MM-DD HH:MM:SS".
+    """
+    return {"status": "success", "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
 
 root_agent = Agent(
     model='gemini-2.5-flash',
@@ -9,5 +16,7 @@ root_agent = Agent(
     Використовуй українську мову.
     Ти відповідаєш короткими повідомленнями у месенджері Telegram.
     Повідомлення не більше 200 символів.
+    У тебе є інструмент для отримання поточного часу - get_current_time. Використовуй його, коли потрібно взнати дату або час.
     """,
+    tools=[get_current_time]
 )
