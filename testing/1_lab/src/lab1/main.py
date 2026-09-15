57ae9193268160fdf9780c9b644d7bd80ad7af32
BobasB <bugil.bogdan@gmail.com>
2026-09-15 12:40:22 +0300

    пройшли asserts

--- testing/1_lab/src/lab1/main.py
+++ testing/1_lab/src/lab1/main.py
@@ -1,6 +1,11 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
 def main(p: int = 5) -> int:
     for i in range(1, p):
-        print(i)
+        logger.debug(f"Processing {i}")
     return 0
 
 if __name__ == "__main__":
--- testing/1_lab/tests/test_main.py
+++ testing/1_lab/tests/test_main.py
@@ -16,7 +16,23 @@ def test():
         print("Test passed")
     else:
         print("Test failed")
+        raise AssertionError("Викликаємо помилку спеціально для перевірки роботи assert")
 
+
+def test_via_assert():
+    assert main() == 0, "Assert / твердження є неправдиве, тест не пройшов функція має повернути 0"
+    #assert False, "Спеціально створена помилка для перевірки роботи assert"
+    # assert не вважається тестом, а лише перевіркою умови, тест складається з безлічі assert
+    for i in [int(0), int(100)]:
+        assert main(i) == 0, "Assert / твердження є неправдиве, тест не пройшов, функція ма єповернути 0"
+        assert isinstance(main(i), int), "Assert / твердження є неправдиве, тест не пройшов"
+
+
+def test_main():
+    print(f"{20*'#'} Починаємо тестування... {20*'#'}")
+    #test()
+    test_via_assert() 
+    print(f"{20*'#'} Тестування завершено. {20*'#'}") 
 
 if __name__ == "__main__":
-    test()
+    test_main()
