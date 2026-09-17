b94989fd94ee029eee2b7e95b2e00c91c4a9aa98
BobasB <bugil.bogdan@gmail.com>
2026-09-17 12:55:56 +0300

    створили декілька юніт тестів

--- testing/1_lab/src/lab1/main.py
+++ testing/1_lab/src/lab1/main.py
@@ -3,6 +3,11 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
+
+def celsius_to_fahrenheit(celsius: float) -> float:
+    return celsius * 9 / 5 + 32
+
+
 def main(p: int = 5) -> int:
     for i in range(1, p):
         logger.debug(f"Processing {i}")
--- testing/1_lab/tests/test_main.py
+++ testing/1_lab/tests/test_main.py
@@ -1,4 +1,6 @@
-from lab1.main import main
+import unittest
+
+from lab1.main import celsius_to_fahrenheit, main
 
 
 def test():
@@ -34,5 +36,52 @@ def test_main():
     test_via_assert() 
     print(f"{20*'#'} Тестування завершено. {20*'#'}") 
 
+
+class TestTemperatureConversion(unittest.TestCase):
+    def test_celsius_to_fahrenheit_with_assets(self) -> None:
+        """Пробуємо через assert"""
+        assert celsius_to_fahrenheit(0.0) == 32.0, f"Expected 32!"
+        assert celsius_to_fahrenheit(100.0) == 212.0, f"Expected 212!"
+        assert celsius_to_fahrenheit(-40.0) == -40.0, f"Expected -40!"
+        assert isinstance(celsius_to_fahrenheit(36.6), float), f"Expected float!"
+        assert abs(celsius_to_fahrenheit(36.6) - 97.88) < 1e-2, f"Expected 97.88!"
+
+    def test_celsius_to_fahrenheit_with_unittest_assets(self) -> None:
+        """Пробуємо через unittest assert"""
+        self.assertEqual(celsius_to_fahrenheit(0.0), 32.0, f"Expected 32!")
+        self.assertEqual(celsius_to_fahrenheit(100.0), 212.0, f"Expected 212!")
+        self.assertEqual(celsius_to_fahrenheit(-40.0), -40.0, f"Expected -40!")
+        self.assertIsInstance(celsius_to_fahrenheit(36.6), float, f"Expected float!")
+        self.assertAlmostEqual(celsius_to_fahrenheit(36.6), 97.88, msg="Expected 97.88!")
+
+    def test_celsius_to_fahrenheit_with_input_data(self) -> None:
+        cases = [
+            (0, 32),
+            (0.0, 32.0),
+            (100.0, 212.0),
+            (-40.0, -40.0),
+            (36.6, 97.88),
+        ]
+        for c, fr in cases:
+            self.assertAlmostEqual(
+                celsius_to_fahrenheit(c), fr, msg=f"Expected {fr} for input {c}"
+            )
+
+    def test_celsius_to_fahrenheit_with_subtests(self) -> None:
+        cases = [
+                    (0, 32),
+                    (0.0, 32.0),
+                    (100.0, 212.0),
+                    (-40.0, -40.0),
+                    (36.6, 97.88),
+                ]
+        for c, fr in cases:
+            with self.subTest(celsius=c):
+                self.assertAlmostEqual(
+                    celsius_to_fahrenheit(c), fr, msg=f"Expected {fr} for input {c}"
+                )
+
+
 if __name__ == "__main__":
-    test_main()
+    #test_main()
+    unittest.main()
